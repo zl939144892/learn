d69604dd833c9694acdaf5ded3007cea5422aa49
zl939144892 <zl939144892@vip.qq.com>
2017-01-02 13:23:34 +0800

    新年第二天~

--- Application/Common/Model/MenuModel.class.php
+++ Application/Common/Model/MenuModel.class.php
@@ -77,4 +77,17 @@ class MenuModel extends Model{
 
 		return $this->_db->where($data)->order('listorder desc, menu_id desc')->select();
 	}
+
+	public function getBarMenus(){
+		$data = array(
+			'status' => array('neq',-1),
+			'type' => 2,
+		);
+
+		$res = $this->_db->where($data)
+			->order('listorder desc, menu_id desc')
+			->select();
+
+		return $res;
+	}
 }
